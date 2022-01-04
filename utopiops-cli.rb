35532eae731194d6d241c001e05c024768222850
Armin Goodarzi <armingodarzi1380@gmail.com>
2022-01-04 22:58:40 +0330

    Update utopiops-cli.rb

--- utopiops-cli.rb
+++ utopiops-cli.rb
@@ -4,8 +4,6 @@ class UtopiopsCli < Formula
   url "https://github.com/Armingodiz/homebrew-utopiops/releases/download/first-release/utopiops-cli-0.0.1.tar.gz"
   sha256 "9cf3b4ea854858a910bb13436165d1c8fd96ec9cfa5748cb41bb1783d7aaf9e0"
 
-  bottle :unneeded
-
   def install
     bin.install "utopiops-cli"
   end
