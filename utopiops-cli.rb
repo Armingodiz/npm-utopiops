ffeb3fa10e7cf67c0a6bcfb460a65ad0d6d9d1e8
Armin Goodarzi <armingodarzi1380@gmail.com>
2022-01-04 23:19:28 +0330

    Update utopiops-cli.rb

--- utopiops-cli.rb
+++ utopiops-cli.rb
@@ -5,7 +5,7 @@ class UtopiopsCli < Formula
   sha256 "9cf3b4ea854858a910bb13436165d1c8fd96ec9cfa5748cb41bb1783d7aaf9e0"
 
   def install
-    bin.install "utopiops-cli"
+    bin.install "utopiops-cli_darwin_amd64"
   end
 
   test do
