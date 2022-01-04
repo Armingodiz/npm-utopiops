da648988ec9c071f7a4c21e46bb539dcbdf95c51
Armin Goodarzi <armingodarzi1380@gmail.com>
2022-01-04 22:20:46 +0330

    Create utopiops-cli.rb

---
+++ utopiops-cli.rb
@@ -0,0 +1,16 @@
+class UtopiopsCli < Formula
+  desc "CLI tool for utopiops.com"
+  homepage "https://github.com/Armingodiz/homebrew-utopiops"
+  url "https://github.com/Armingodiz/homebrew-utopiops/releases/download/first-release/utopiops-cli-0.0.1.tar.gz"
+  sha256 "9cf3b4ea854858a910bb13436165d1c8fd96ec9cfa5748cb41bb1783d7aaf9e0"
+
+  bottle :unneeded
+
+  def install
+    bin.install "utopiops-cli"
+  end
+
+  test do
+    system "#{bin}/utopiops-cli", "--version"
+  end
+end
